1af78d34784a8ff2653f5c4ec36817205b62535d
Bryce B <bryce.boddy@gmail.com>
2021-01-04 23:37:59 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+let wakeDog = function(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+
+let leashDog = function(dogName, dogBreed) {
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+}
+
+let walkToPark = function(dogName, dogBreed) {
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+}
+
+let throwFrisbee = function(dogName, dogBreed) {
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+}
+
+let walkHome = function(dogName, dogBreed) {
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+}
+
+let unleashDog = function(dogName, dogBreed) {
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+}
+
+let exerciseDog = function(dogName, dogBreed) {
+  
+}
