fedf3cdfe7096bb0ee65938a2be8db36bb53fee6
Bryce B <bryce.boddy@gmail.com>
2021-01-04 23:51:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-let wakeDog = function(dogName, dogBreed) {
+var wakeDog = function(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
-let leashDog = function(dogName, dogBreed) {
+var leashDog = function(dogName, dogBreed) {
   console.log(`Leash ${dogName} the ${dogBreed}`);
 }
 
-let walkToPark = function(dogName, dogBreed) {
+var walkToPark = function(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
 }
 
-let throwFrisbee = function(dogName, dogBreed) {
+var throwFrisbee = function(dogName, dogBreed) {
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
 }
 
-let walkHome = function(dogName, dogBreed) {
+var walkHome = function(dogName, dogBreed) {
   console.log(`Walk home with ${dogName} the ${dogBreed}`);
 }
 
-let unleashDog = function(dogName, dogBreed) {
+var unleashDog = function(dogName, dogBreed) {
   console.log(`Unleash ${dogName} the ${dogBreed}`);
 }
 
-let exerciseDog = function(dogName, dogBreed) {
+var exerciseDog = function(dogName, dogBreed) {
   
 }
